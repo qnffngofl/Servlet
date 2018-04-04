adfe661f5b3af2c71d4602e7ac6d44319e50e404
GOODEE <GOODEE@DESKTOP-AQTK6R2>
2018-04-04 13:38:43 +0900

    # WARNING: head commit changed in the meantime
    파라메터 테스트 완료!(GET & POST)

--- src/kr/gudi/Main.java
+++ src/kr/gudi/Main.java
@@ -1,6 +1,7 @@
 package kr.gudi;
 
 import java.io.IOException;
+import java.util.Enumeration;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -12,11 +13,25 @@ public class Main extends HttpServlet {
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		System.out.println("doGet");
+		getParam(req);
+		
+		
 	}
 
 	@Override
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		System.out.println("doPost");
+		req.setCharacterEncoding("UTF-8");
+		getParam(req);
+		
 	}
 	///post방식을 쓰려면 주소를 보낼 새로운 화면이 필요하다. get방식은 그냥 쓰면 된다 참고로.
+	void getParam(HttpServletRequest req) {
+		Enumeration<String> names = req.getParameterNames();
+		while (names.hasMoreElements()) {
+			String paramName = (String) names.nextElement();
+			String value = req.getParameter(paramName);
+			System.out.println(paramName + " : " + value);
+		}
+	}
 }
